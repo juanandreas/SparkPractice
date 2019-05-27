adc8d77414ac4970ab0283cf73ba91b0b6ee237c
juanandreas-acc <juan.andreas@accolade.com>
2019-05-27 02:02:23 -0700

    bleh

--- play.py
+++ play.py
@@ -1,5 +1,23 @@
-from spark_session import *
+from pyspark.sql import SparkSession
+
+spark = SparkSession\
+    .builder\
+    .appName("play")\
+    .getOrCreate()
 
 food_df = spark.read.csv("food.csv", inferSchema=True, header=True)
 food_df.show()
+
+
+
+food_df.createOrReplaceTempView("food_df")
+
+df = spark.sql("""
+    SELECT * from food_df
+    WHERE Type = 'Animal'
+""")
+
+df.show()
+
+spark.stop()
 
